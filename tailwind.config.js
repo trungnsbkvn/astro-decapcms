ba7d745f87a3b7b3a0d33814c43fcc3a6378e84f
Daniel Nguyen <daniel@digitalx.net.au>
2024-11-27 16:03:35 +0700

    update zalo, call

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,12 +21,34 @@ export default {
 
       animation: {
         fade: 'fadeInUp 1s both',
+        call: 'play 1.5s ease infinite'
       },
 
       keyframes: {
         fadeInUp: {
           '0%': { opacity: 0, transform: 'translateY(2rem)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        play: {
+          '0%': {
+            transform: 'rotate(0deg) scale(1) skew(1deg)'
+          },
+          '10%': {
+            transform: 'rotate(-25deg) scale(1) skew(1deg)'
+          },
+          '20%': {
+            transform: 'rotate(25deg) scale(1) skew(1deg)',
+            'box-shadow': '0 0 0 5px #09568a,0 0 0 10px #bedaf8'
+          },
+          '30%': {
+            transform: 'rotate(-25deg) scale(1) skew(1deg)'
+          },
+          '40%': {
+            transform: 'rotate(25deg) scale(1) skew(1deg)'
+          },
+          '100%': {
+            transform: 'rotate(0deg) scale(1) skew(1deg)'
+          },
         },
       },
     },
